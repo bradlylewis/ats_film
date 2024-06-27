6a8dde1958a6aaa661b1d7db10bdca6f74ab3f9d
Bradly Lewis <bradlylewisdev@gmail.com>
2024-06-27 12:53:32 -0400

    complete tablet breakpoint

--- script.js
+++ script.js
@@ -16,9 +16,42 @@ const config = {
   type: 'carousel',
   autoplay: 5000,
   hoverpause: true,
-  peek: 200,
+  // peek: 100,
   
 }
 document.addEventListener('DOMContentLoaded', function() {
     new Glide('.glide', config).mount()
 });
+
+
+// Get the modal
+var modal = document.getElementById("imageModal");
+
+// Get the modal image
+var modalImg = document.getElementById("modalImage");
+
+// Get all images in the carousel
+var images = document.querySelectorAll(".glide__slide img");
+
+// Loop through the images and add the click event
+images.forEach(function(image) {
+  image.addEventListener("click", function() {
+    modal.classList.add("show");
+    modalImg.src = this.src;
+  });
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.classList.remove("show");
+}
+
+// Close the modal when clicking outside of the image
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.classList.remove("show");
+  }
+}
